93ab5899baf83d9db2d3708a271df3de0bdeca7e
yared-abera <yaredoabera7@gmail.com>
2024-10-18 15:15:10 +0300

    this first commit for netflix-clone-app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCFIQOtBy5PSLwaIL8Lb0LhaYSbBQegj8M",
+    authDomain: "react-netflix-clone-651d9.firebaseapp.com",
+    projectId: "react-netflix-clone-651d9",
+    storageBucket: "react-netflix-clone-651d9.appspot.com",
+    messagingSenderId: "863246719875",
+    appId: "1:863246719875:web:4f648114efb40dea8b34b6"
+  };
